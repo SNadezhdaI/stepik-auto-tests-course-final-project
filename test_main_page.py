6df6c5de819470f0866ab6ca5cd4e84a57d6476d
Nadezhda <79163793388@yandex.ru>
2021-10-21 13:08:38 +0300

    Add new test for login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
@@ -10,3 +11,12 @@ def test_guest_can_go_to_login_page(browser):
     page.open()
     page.go_to_login_page()
     page.should_be_login_link()
+    
+def test_login_and_registration_form_is_presented(browser):
+    link_login = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page_login = LoginPage(browser, link_login)
+    page_login.open()
+    page_login.should_be_login_page()
+    
+    
+    
